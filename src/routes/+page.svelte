993bf9c5ad94f3d30af224195eb2159b32b9e005
Joakim <132438695+jxakim@users.noreply.github.com>
2026-05-20 12:55:55 +0200

    Added a background view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <main
-	class="min-h-screen bg-slate-900 text-slate-100"
+	class="min-h-screen text-slate-100"
 >
 	<nav class="fade-in pt-10" style="--fade-delay: 40ms">
 		<div class="flex w-full items-center px-6">
