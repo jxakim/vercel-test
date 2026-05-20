ff3ee7b4adbc85939dc7f54bab1fd8a108cd33b6
Joakim <132438695+jxakim@users.noreply.github.com>
2026-05-20 11:08:00 +0200

    Fixed some size problems

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,16 @@
 <main
 	class="min-h-screen bg-slate-900 text-slate-100"
 >
-	<nav class="pt-20 pl-20 ">
+	<nav class="pt-10 pl-5 ">
 		<div class="flex w-full items-center justify-start gap-8 px-6">
 			<a
 				href="/"
 				class="inline-flex transition duration-200 hover:-translate-y-0.5 hover:scale-[1.02]"
 			>
-				<img src={logo} alt="Kontrast Web Logo" class="w-14 sm:w-16 md:w-30" />
+				<img src={logo} alt="Kontrast Web Logo" class="w-14 sm:w-16 md:w-16" />
 			</a>
 
-			<ul class="flex w-fit items-center gap-5 text-xl font-medium text-slate-300 sm:gap-10 sm:text-3xl">
+			<ul class="flex w-fit items-center gap-5 text-xl font-medium text-slate-300 sm:gap-10 sm:text-md">
 				<li><a href="/" class="transition hover:text-white">Home</a></li>
 				<li><a href="/" class="transition hover:text-white">Portfolio</a></li>
 				<li><a href="/" class="transition hover:text-white">About</a></li>
@@ -24,7 +24,7 @@
 	</nav>
 
 	<section class="mx-auto flex w-full max-w-5xl flex-col items-center gap-6 px-6 py-20 text-center sm:gap-10 sm:px-20">
-        <h1 class="text-4xl font-bold sm:text-5xl md:text-6xl">Welcome to Kontrast Web</h1>
-        <p class="text-lg text-slate-400 sm:text-xl md:text-2xl">We are under construction of our website, please come back later.</p>
+        <h1 class="text-2xl font-bold sm:text-2xl md:text-4xl">Welcome to Kontrast Web</h1>
+        <p class="text-lg text-slate-400 sm:text-xl md:text-xl">Kontrast web is under construction. Please check back later.</p>
 	</section>
 </main>
